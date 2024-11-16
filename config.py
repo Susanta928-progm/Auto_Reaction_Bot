4cccbf95386de53f334e2fd58703e6746f6c94ef
Susanta928-progm <susantasarkar1906@gmail.com>
2024-11-17 00:44:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 from os import environ as env
 
 class Config:
-    API_ID = int(env.get("API_ID", "")) #TG API ID
-    API_HASH = env.get("API_HASH", "") #TG API HASH
-    BOT_TOKEN = env.get("BOT_TOKEN", "") #Add Bot Token, get from botfather
-    FSUB_ID = int(env.get("FSUB_ID", ""))  #Add Your FSub Channel Id -100xxxxxxxxx
+    API_ID = int(env.get("API_ID", "21134445")) #TG API ID
+    API_HASH = env.get("API_HASH", "231c18ea7273824491d6bf05425ab74e
+") #TG API HASH
+    BOT_TOKEN = env.get("BOT_TOKEN", "7517513084:AAHMla-mLgYS11aeGXmk8c_4EWZYvNYk6TM") #Add Bot Token, get from botfather
+    FSUB_ID = int(env.get("FSUB_ID", "-1002099263061"))  #Add Your FSub Channel Id -100xxxxxxxxx
     FSUB = bool(env.get("FSUB", True)) #Keep True If U Want Force Subscribe         
     SB_PIC = env.get("SB_PIC", "https://i.ibb.co/c6vkVBP/photo-2024-11-15-12-13-41-7437478159336865812.jpg") #Add Link For Start Cmd Pic
-    BOT_USERNAME = env.get("BOT_USERNAME", "Sb_reactionbot") #Add Bot Username Without @
+    BOT_USERNAME = env.get("BOT_USERNAME", "SST_REACTIONBOT") #Add Bot Username Without @
     EMOJIS = [
         "👍", "👎", "❤", "🔥", 
         "🥰", "👏", "😁", "🤔",
